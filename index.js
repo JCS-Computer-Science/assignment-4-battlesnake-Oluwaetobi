6464ee04e9acd9022db175af2c5aec93fa7cd8a7
Oluwaetobi Ogbon <paull41@educbe.ca>
2025-04-14 16:42:48 +0000

    I was supposed to push this in last week but it didn't work, so I am doing it now, Monday, April 14, 2025

--- index.js
+++ index.js
@@ -9,8 +9,9 @@
 //
 // To get you started we've included code to prevent your Battlesnake from moving backwards.
 // For more info see docs.battlesnake.com
-import express from 'express';
+import express, { request, response } from 'express';
 import move from './moveLogic.js'
+
 
 const app = express();
 app.use(express.json());
@@ -18,23 +19,36 @@ const config = {
   apiversion: "1",
   author: "",       // TODO: Your Battlesnake Username
   color: "#cb82ff", // TODO: Choose color
-  head: "default",  // TODO: Choose head, see https://play.battlesnake.com/customizations/ for options unlocked in your account
+  head: "safe",  // TODO: Choose head, see https://play.battlesnake.com/customizations/ for options unlocked in your account
   tail: "default",  // TODO: Choose tail, see https://play.battlesnake.com/customizations/ for options unlocked in your account
 }
 
 //TODO: respond to GET requests on "/" with the config object above
+app.get("/", (request,response) => {
+  response.json(config);
+});
 
 //TODO: respond to POST requests on "/start". Your response itself is ignored, but must have status code "200"
 //      the request body will contain objects representing the game instance, game board state, and your snake
 //      https://docs.battlesnake.com/api/requests/start
+app.post("/start", (request,response) => {
+  response.status(200).json("OK");
+})
 
 //TODO: respond to POST requests on "/move". Your response should be an object with a "move" property and optionally
 //      a "shout" property. The request body again contains objects representing the game state
 //      https://docs.battlesnake.com/api/requests/move
+app.post("/move", (request,response) => {
+  let movement = move(request.body);
+  response.status(200).json(movement);
+})
 
 //TODO: respond to POST requests on "/end", which signals the end of a game. Your response itself is ignored, 
 //      but must have status code "200" the request body will contain objects representing the game
 //      https://docs.battlesnake.com/api/requests/end
+app.post("/end", (request,response) => {
+  response.status(200).json("OK");
+})
 
 const host = '0.0.0.0';
 const port = process.env.PORT || 8000;
