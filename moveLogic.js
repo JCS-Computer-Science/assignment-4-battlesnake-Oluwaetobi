557e6316e39598ba482acca54beeee5bf9a4333f
Oluwaetobi Ogbon <paull41@educbe.ca>
2025-05-17 17:04:37 +0000

    Mo ti se ilosiwaju

--- moveLogic.js
+++ moveLogic.js
@@ -2078,8 +2078,9 @@ export default function move(gameState){
             }
 
             let deadEndPunishmentPoints = (theSamePunishment + 1);
-            // I don't want it to be to big here, because it might not be the right decision
-            let moveAwayFromBorderPoints = 1;
+            // I don't want it to be to big here, because it might not be the right decision, please don't change the 
+            // number here because this is the sweet spot, "2"
+            let moveAwayFromBorderPoints = 2;
     
            // INSERTED BACK STUFF
            let distanceFromBorder = 2;
